09e62b83b2e38120875fa91d2102d0d9e60d19c4
Stacy <stacychapa@hotmail.com>
2026-06-01 17:51:05 -0600

    Implementación de process.env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
+const express = require('express');
+const app = express();
+
+const ENV = process.env.NODE_ENV || 'development';
+console.log(`🚀 Running in ${ENV} mode`);
+
 
 /* const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./src/config/swagger'); */
