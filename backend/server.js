160e8b8cb337714cefaefebc82cb1b551bb7c540
Stacy <stacychapa@hotmail.com>
2026-06-01 17:58:09 -0600

    Server js and package.json fixed routes

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,14 @@
 require('dotenv').config();
+const express = require('express');
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
-const express = require('express');
-const app = express();
 
 const ENV = process.env.NODE_ENV || 'development';
 console.log(`🚀 Running in ${ENV} mode`);
 
-
-/* const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./src/config/swagger'); */
-
 connectDB();
 
-/* app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec)); */
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
 });
-
